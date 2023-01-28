7e6c0c880c95c71dba8a7d0c00db48288cf2ef8a
devlikebear <devlikebear@gmail.com>
2023-01-28 19:21:50 +0900

    feat: npmjs 배포를 위한 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
+
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      name: 'DevforestVueAnalogClock',
+      fileName: 'devforest-vue-analog-clock'
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
 })
